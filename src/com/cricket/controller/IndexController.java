6b4260836c5e5628e48c672c444bb1584bc543a6
Jaber Shaikh <ShaikhJ@regents.ac.uk>
2023-11-18 13:16:30 +0530

    Session variables added

--- src/com/cricket/controller/IndexController.java
+++ src/com/cricket/controller/IndexController.java
@@ -8,10 +8,12 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.cricket.model.Configuration;
@@ -21,14 +23,14 @@ import com.cricket.util.CricketUtil;
 
 import net.sf.json.JSONObject;
 
+@SessionAttributes(value = {"session_speed", "session_configuration"})
 @Controller
 public class IndexController 
 {
-	Speed session_speed;
-	Configuration session_configuration;
-	
 	@RequestMapping(value = {"/"}, method={RequestMethod.GET,RequestMethod.POST}) 
-	public String initialisePage(ModelMap model) throws JAXBException
+	public String initialisePage(ModelMap model,
+			@ModelAttribute("session_speed") Speed session_speed,
+			@ModelAttribute("session_configuration") Configuration session_configuration) throws JAXBException
 	{
 		if(new File(CricketUtil.CRICKET_DIRECTORY + CricketUtil.CONFIGURATIONS_DIRECTORY + CricketUtil.SPEED_XML).exists()) {
 			session_configuration = (Configuration)JAXBContext.newInstance(Configuration.class).createUnmarshaller().unmarshal(
@@ -38,18 +40,26 @@ public class IndexController
 			JAXBContext.newInstance(Configuration.class).createMarshaller().marshal(session_configuration, 
 					new File(CricketUtil.CRICKET_DIRECTORY + CricketUtil.CONFIGURATIONS_DIRECTORY + CricketUtil.SPEED_XML));
 		}
+		model.addAttribute("session_speed",session_speed);
 		model.addAttribute("session_configuration",session_configuration);
+		session_speed = new Speed(0);
 		return "initialise";
 	}
 
 	@RequestMapping(value = {"/output"}, method={RequestMethod.GET,RequestMethod.POST}) 
-	public String outputPage(ModelMap model)
+	public String outputPage(ModelMap model,
+			@ModelAttribute("session_speed") Speed session_speed,
+			@ModelAttribute("session_configuration") Configuration session_configuration)
 	{
+		model.addAttribute("session_speed",session_speed);
+		model.addAttribute("session_configuration",session_configuration);
 		return "speed";
 	}
 	
 	@RequestMapping(value = {"/upload_initialise_data"}, method={RequestMethod.GET,RequestMethod.POST})    
-		public @ResponseBody String uploadFormDataToSessionObjects(MultipartHttpServletRequest request) throws JAXBException 
+		public @ResponseBody String uploadFormDataToSessionObjects(MultipartHttpServletRequest request,
+		@ModelAttribute("session_speed") Speed session_speed,
+		@ModelAttribute("session_configuration") Configuration session_configuration) throws JAXBException 
 	{
 		
 		for (Entry<String, String[]> entry : request.getParameterMap().entrySet()) {
@@ -76,8 +86,9 @@ public class IndexController
 	@RequestMapping(value = {"/processCricketProcedures"}, method={RequestMethod.GET,RequestMethod.POST})    
 	public @ResponseBody String processCricketProcedures(
 			@RequestParam(value = "whatToProcess", required = false, defaultValue = "") String whatToProcess,
-			@RequestParam(value = "valueToProcess", required = false, defaultValue = "") String valueToProcess) 
-			throws JAXBException, IOException
+			@RequestParam(value = "valueToProcess", required = false, defaultValue = "") String valueToProcess,
+			@ModelAttribute("session_speed") Speed session_speed,
+			@ModelAttribute("session_configuration") Configuration session_configuration) throws JAXBException, IOException
 	{	
 		switch (whatToProcess.toUpperCase()) {
 		case "SPEED":
@@ -85,11 +96,10 @@ public class IndexController
 			if(session_speed == null) {
 				session_speed = new Speed(0);
 			}
-			Speed this_speed = CricketFunctions.saveCurrentSpeed(session_configuration.getBroadcaster().toUpperCase(), 
+
+			session_speed = CricketFunctions.saveCurrentSpeed(session_configuration.getBroadcaster().toUpperCase(), 
 					session_configuration.getPrimaryIpAddress(), session_configuration.getFilename(), session_speed);
-			if(this_speed != null) {
-				session_speed = this_speed;
-			}
+
 			return JSONObject.fromObject(session_speed).toString();
 
 		default:
@@ -98,4 +108,14 @@ public class IndexController
 		
 		}
 	}
+
+	@ModelAttribute("session_speed")
+	public Speed session_speed(){
+		return new Speed();
+	}
+	@ModelAttribute("session_configuration")
+	public Configuration session_configuration(){
+		return new Configuration();
+	}
+	
 }
